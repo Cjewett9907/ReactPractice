cf0caef7bec5e4dc9e7fed05fe07a15cf1c4eca8
Cjewett_work <Craig9907@gmail.com>
2019-06-05 08:54:17 -0700

    update added user greet functionality

--- react_practice1.js
+++ react_practice1.js
@@ -2,24 +2,50 @@ import React from 'react'
 import ReactDOM from 'react'
 // import other necessary resources
 
+class WelcomeUser extends React.Component {
+    handleNameChange = (e) => {
+      this.props.updateUserName(e.target.value);
+    };
+    
+    render() {
+      return (
+        <input type='text' placeholder='Enter your name' 
+               value={this.props.name}
+               onChange={this.handleNameChange} />
+      );
+    }
+  }
+
+
 
 class helloWorld extends React.Component {
     constructor(props){
         super(props)
-        state = {
+        this.state = {
             name: "",
             greet: "Hello World",
             todoList: ["first", "second", "third"]
         }
-
-
     }
+
+    updateUserName = (n) => {
+        this.setState({ name: name });
+      };
+
+    clearText = () => {
+        this.setState({ name: "" })
+      };
 
 
     render(){
         return(
             <div className="hello-world-div">
                 <h1>{this.state.greet}</h1>
+
+                <WelcomeUser name={this.state.name}
+                     updateUserName={this.updateUserName} />
+                    <button type='button' onClick={this.clearText}>Clear</button>
+                <br />
 
 
                 {this.state.todoList.map (item => (
